09955ce75a268631f57edcd70ab84016d3eb6433
Jessica <jesscara.993@gmail.com>
2023-02-13 17:33:39 +0100

    printed user detail on page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,15 @@
 
 @section('content')
   <h1 class="text-center">Contenuto pagina</h1>
+  <ul>
+    @foreach ($people as $person)
+        <li class="my-3">
+          User: {{$person -> name}} <br> 
+          Email: {{$person -> mail}} <br>
+          Phone: {{$person -> personDetail -> phone}} <br>
+          Address: {{$person -> personDetail -> address}} <br>
+          <img src="{{$person -> personDetail -> avatar}}" alt=""> <br>
+        </li>
+    @endforeach
+  </ul>
 @endsection
