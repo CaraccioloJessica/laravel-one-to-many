7433f147cc6a6a7c7151cee1c05ddba87ba79071
Jessica <jesscara.993@gmail.com>
2023-02-13 17:16:19 +0100

    personDetail faker and seeder function

---
+++ database/seeders/PersonDetailSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Person;
+use App\Models\PersonDetail;
+
+class PersonDetailSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    Person::all()->each(function ($p) {
+
+      $personDetail = PersonDetail::factory()->make();
+      $personDetail->person()->associate($p);
+
+      $personDetail->save();
+    });
+  }
+}
